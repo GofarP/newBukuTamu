98caa4911d91b07d441e3b699479e757e9ceaf3a
Gofar.P <gofarperdana83@gmail.com>
2021-05-04 00:40:28 +0700

    recode koneksi

--- src/koneksi/koneksi.java
+++ src/koneksi/koneksi.java
@@ -6,25 +6,43 @@
 package koneksi;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 /**
  *
  * @author Gofur
  */
-public class koneksi {
+
+public class koneksi
+{
     private static Connection mysqlconfig;
-    public static Connection configDB()throws SQLException{
-        try {
-            String url="jdbc:mysql://localhost:3306/bukutamu"; //url database
-            String user="root"; //user database
-            String pass=""; //password database
-            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
-            mysqlconfig=DriverManager.getConnection(url, user, pass);            
-        } catch (Exception e) {
-            System.err.println("koneksi gagal "+e.getMessage()); //perintah menampilkan error pada koneksi
+    public static  Connection configDB() throws SQLException
+    {
+        try 
+        {
+            String url="jdbc:mysql://localhost:3306/bukutamu";
+            String user="root";
+            String pass="";
+        } 
+        
+        catch (Exception e) 
+        {
+            
         }
-        return mysqlconfig;
-    }    
+    }
 }
+//public class koneksi {
+//    private static Connection mysqlconfig;
+//    public static Connection configDB()throws SQLException{
+//        try {
+//            String url="jdbc:mysql://localhost:3306/bukutamu"; //url database
+//            String user="root"; //user database
+//            String pass=""; //password database
+//            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+//            mysqlconfig=DriverManager.getConnection(url, user, pass);            
+//        } catch (Exception e) {
+//            System.err.println("koneksi gagal "+e.getMessage()); //perintah menampilkan error pada koneksi
+//        }
+//        return mysqlconfig;
+//    }    
+//}
